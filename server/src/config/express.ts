7c621dffa7ad8de60819cbbaa616b94808455ce5
AmitSahar16 <amitsahar16@gmail.com>
2026-03-13 23:06:17 +0200

    add empty router

--- server/src/config/express.ts
+++ server/src/config/express.ts
@@ -2,6 +2,7 @@ import { Express } from 'express';
 import cors from 'cors';
 import helmet from 'helmet';
 import rateLimit from 'express-rate-limit';
+import routes from '../routes';
 import { errorHandler } from '../middleware/errorHandler';
 import express from 'express';
 import path from 'path';
@@ -25,6 +26,8 @@ export const configExpress = (app: Express, port: number) => {
   app.use(express.json());
 
   app.use('/uploads', express.static(path.join(__dirname, '../../uploads')));
+
+  app.use('/', routes);
 
   app.use(errorHandler);
 };
